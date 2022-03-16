d26a04d055077e303fad1552e584dfd26df46fa6
ong-ck <ongck0808@gmail.com>
2022-03-16 14:51:46 +0800

    Google sign in functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js'; // https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js
-import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js'; // https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js
+import { getAuth, 
+         onAuthStateChanged,
+         GoogleAuthProvider,
+         signInWithPopup,
+         signOut,
+       } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js'; // https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js
+
 
 const firebaseApp = initializeApp({
   apiKey: "AIzaSyAabIrGuFwV69dJ2XsB5S5tEZ5vijR2Mc8",
@@ -10,7 +16,44 @@ const firebaseApp = initializeApp({
   appId: "1:857224886725:web:14ae557f4292140be973ac",
   measurementId: "G-6J0EX5MG30"
 });
-const auth = getAuth(firebaseApp);
+// const auth = getAuth(firebaseApp);
+const provider = new GoogleAuthProvider(firebaseApp);
+const auth = getAuth();
+
+document.getElementById("signout").style.visibility = "hidden";
+
+document.getElementById("signin").addEventListener("click", () => {
+  signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // ...
+    document.getElementById("signin").style.visibility = 'hidden';
+    document.getElementById("signout").style.visibility = 'visible';
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+});
+
+document.getElementById("signout").addEventListener("click", () => {
+  signOut(auth).then(() => {
+    // Sign-out successful.
+    document.getElementById("signin").style.visibility = 'visible';
+    document.getElementById("signout").style.visibility = 'hidden';
+  }).catch((error) => {
+    // An error happened.
+  });
+});
 
 onAuthStateChanged(auth, user => {
   if (user != null) {
