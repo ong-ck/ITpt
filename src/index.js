42740475a94e7d1785d2c9f901da63840b0a0a57
ong-ck <ongck0808@gmail.com>
2022-06-23 11:33:38 +0800

    Updated ics export to new event object, and update of calendar with stored events upon login

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js'; // https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js"; // https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js
 import {
   getAuth,
   onAuthStateChanged,
   GoogleAuthProvider,
   signInWithPopup,
   signOut,
-} from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js'; // https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js
+} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js"; // https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js
 
-import { getFirestore,
-         collection, 
-         addDoc
+import {
+  getFirestore,
+  collection,
+  setDoc,
+  doc,
+  deleteDoc,
+  getDocs,
 } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
 
-import { 
-  score, 
-  get_age_group, 
-  get_push_up, 
-  get_sit_up, 
-  get_run_row, 
+import {
+  score,
+  get_age_group,
+  get_push_up,
+  get_sit_up,
+  get_run_row,
   get_run_score,
   get_add_push_up,
   get_add_sit_up,
@@ -31,15 +35,34 @@ const firebaseApp = initializeApp({
   storageBucket: "itpt-d53e0.appspot.com",
   messagingSenderId: "857224886725",
   appId: "1:857224886725:web:14ae557f4292140be973ac",
-  measurementId: "G-6J0EX5MG30"
+  measurementId: "G-6J0EX5MG30",
 });
 
 // const auth = getAuth(firebaseApp);
 const provider = new GoogleAuthProvider(firebaseApp);
 const auth = getAuth();
 const user = auth.currentUser;
+var user_id = null;
 
 const db = getFirestore();
+
+function db_add(activity) {
+  let activity_title = activity["title"];
+  let activity_start = activity["start"];
+  let activity_id = activity["id"];
+  let activity_time = activity["extendedProps"]["time"];
+  let activity_desc = activity["extendedProps"]["description"];
+
+  const docRef = setDoc(doc(db, "users", user_id, "activities", activity_id), {
+    id: activity_id,
+    title: activity_title,
+    start: activity_start,
+    extendedProps: {
+      time: activity_time,
+      description: activity_desc,
+    },
+  });
+}
 
 /**
  * Calendar function
@@ -63,25 +86,53 @@ function UIDgen() {
 function exportCalendar() {
   var filenametrue = "myCalendar";
   filenametrue = filenametrue.replace(/[_\W]+/g, "_") + ".ics";
-  var icsContent = "BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//itpt.ml//ITpt\r\n" + 
-                   "CALSCALE:GREGORIAN\r\nBEGIN:VTIMEZONE\r\nTZID:Asia/Shanghai\r\n" + 
-                   "LAST-MODIFIED:20220622T051600Z\r\n" + 
-                   "TZURL:http://tzurl.org/zoneinfo-outlook/Asia/Shanghai\r\n" + 
-                   "X-LIC-LOCATION:Asia/Shanghai\r\nBEGIN:STANDARDTZNAME:CST\r\n" + 
-                   "TZOFFSETFROM:+0800\r\nTZOFFSETTO:+0800\r\nDTSTART:19700101T000000\r\n" + 
-                   "END:STANDARD\r\nEND:VTIMEZONE\r\n";
-                   
+  var icsContent =
+    "BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//itpt.ml//ITpt\r\n" +
+    "CALSCALE:GREGORIAN\r\nBEGIN:VTIMEZONE\r\nTZID:Asia/Singapore\r\n" +
+    "LAST-MODIFIED:20201011T015911Z\r\n" +
+    "TZURL:http://tzurl.org/zoneinfo-outlook/Asia/Singapore\r\n" +
+    "X-LIC-LOCATION:Asia/Singapore\r\nBEGIN:STANDARD\r\nTZNAME:+08\r\n" +
+    "TZOFFSETFROM:+0800\r\nTZOFFSETTO:+0800\r\nDTSTART:19700101T000000\r\n" +
+    "END:STANDARD\r\nEND:VTIMEZONE\r\n";
+
   for (let i = 0; i < allEvents.length; i += 1) {
     console.log(allEvents[i]);
-    let timestamp = allEvents[i]["start"].replaceAll("-", "") + "T000000Z"; // example format: 20220621T210536Z
-    let start = allEvents[i]["start"].replaceAll("-", "");
+    let date = new Date();
+    let timestamp =
+      date.getFullYear() +
+      ("0" + (date.getMonth() + 1)).slice(-2) +
+      ("0" + date.getDate()).slice(-2) +
+      "T" +
+      ("0" + date.getHours()).slice(-2) +
+      ("0" + date.getMinutes()).slice(-2) +
+      ("0" + date.getSeconds()).slice(-2) +
+      "Z";
+    let start =
+      allEvents[i]["start"].replaceAll("-", "") +
+      "T" +
+      allEvents[i]["extendedProps"]["time"] +
+      "00"; // example format: 20220621T210536Z
     let title = allEvents[i]["title"];
     let uid = allEvents[i]["id"];
+    let description = allEvents[i]["extendedProps"]["description"];
 
-    icsContent += "BEGIN:VEVENT\r\nDTSTAMP:" + timestamp + "\r\n" + 
-                  "UID:" + uid + "\r\n" + 
-                  "DTSTART;VALUE=DATE:" + start + "\r\nDTEND;VALUE=DATE:" + start + "\r\n" + 
-                  "SUMMARY:Pushup\r\nEND:VEVENT\r\n";
+    icsContent +=
+      "BEGIN:VEVENT\r\nDTSTAMP:" +
+      timestamp +
+      "\r\n" +
+      "UID:" +
+      uid +
+      "\r\n" +
+      "DTSTART;VALUE=DATE:" +
+      start +
+      "\r\nDTEND;VALUE=DATE:" +
+      start +
+      "\r\n" +
+      "SUMMARY:" +
+      title +
+      "\r\nDESCRIPTION:" +
+      description +
+      "\r\nEND:VEVENT\r\n";
   }
 
   icsContent += "END:VCALENDAR";
@@ -104,14 +155,14 @@ function exportCalendar() {
  * @param {*} allEvents Array that holds all the event objects that will be added by the user.
  */
 function initCalendar(allEvents) {
-  var calendarEl = document.getElementById('calendar');
+  var calendarEl = document.getElementById("calendar");
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'dayGridMonth',
-    initialDate: '2022-06-01',
+    initialView: "dayGridMonth",
+    initialDate: "2022-06-01",
     headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'exportCalendar dayGridMonth,timeGridWeek,timeGridDay'
+      left: "prev,next today",
+      center: "title",
+      right: "exportCalendar dayGridMonth,timeGridWeek,timeGridDay",
     },
     selectable: true,
     events: allEvents,
@@ -127,49 +178,67 @@ function initCalendar(allEvents) {
     },
 
     //Add events
-    select: function(info) {
-      let titleStr = prompt('Enter the activity');
-      let start_date = moment(info.startStr).format('YYYY-MM-DD');
-      let timeStr = prompt('Enter the time of the activity in 24hrs format');
-      let descriptStr = prompt('Enter the details of the activity');
-      
+    select: function (info) {
+      let titleStr = prompt("Enter the activity");
+      let start_date = moment(info.startStr).format("YYYY-MM-DD");
+      let timeStr = prompt("Enter the time of the activity in 24hrs format");
+      let descriptStr = prompt("Enter the details of the activity");
+
       let e = {
+        id: UIDgen(),
         title: titleStr,
         start: start_date,
         extendedProps: {
           time: timeStr,
-          description: (descriptStr == null) ? "Nill" : descriptStr,
-          id: UIDgen(),
-        }
+          description: descriptStr == null ? "Nill" : descriptStr,
+        },
       };
 
       if (e.title != null) {
         allEvents.push(e);
         calendar.addEvent({
-          ...e
+          ...e,
         });
+
+        // Add specific activity to Firestore
+        if (user_id != null) {
+          db_add(e);
+        }
       }
     },
 
     //Select and Delete events
-    eventClick: function(info) {
+    eventClick: function (info) {
       let a = calendar.getEventById(info.event.id);
 
       //Updates details in event popup
       $("#modalLabel").empty().prepend(info.event.title);
-      $("#activity_date").empty().prepend("Date: "+ moment(info.event.start).format('Do MMMM YYYY'));
-      $("#activity_time").empty().prepend("Time: "+ info.event.extendedProps.time);
-      $("#activity_description").empty().prepend("Details: "+ info.event.extendedProps.description);
-      
+      $("#activity_date")
+        .empty()
+        .prepend("Date: " + moment(info.event.start).format("Do MMMM YYYY"));
+      $("#activity_time")
+        .empty()
+        .prepend("Time: " + info.event.extendedProps.time);
+      $("#activity_description")
+        .empty()
+        .prepend("Details: " + info.event.extendedProps.description);
+
       //Toggles modal which dispalys the event info.
-      $('#eventForm').modal('toggle');
-      
+      $("#eventForm").modal("toggle");
+
       //Delete Event
-      $('#delBtn').click(function(){  
+      $("#delBtn").click(function () {
         a.remove();
-        allEvents = allEvents.filter(data => data.id != info.event.id); 
-        $('#eventForm').modal('toggle');
-      });     
+        allEvents = allEvents.filter((data) => data.id != info.event.id); // filter out deleted data
+
+        // Deletes clicked activity from Firestore
+        if (user_id != null) {
+          let doc_id = info.event.id;
+          deleteDoc(doc(db, "users", user_id, "activities", doc_id));
+        }
+
+        $("#eventForm").modal("toggle");
+      });
     },
   });
   calendar.render();
@@ -195,20 +264,18 @@ $(document).ready(function () {
   $(".rewards").hide();
   $("#result").hide();
 
-  $('ul.navbar-left > li')
-    .click(function (e) {
-      $('ul.navbar-nav > li')
-        .removeClass('active');
-      $(this).addClass('active');
-    });
+  $("ul.navbar-left > li").click(function (e) {
+    $("ul.navbar-nav > li").removeClass("active");
+    $(this).addClass("active");
+  });
 
-  $("form").submit(function(event){
+  $("form").submit(function (event) {
     event.preventDefault();
-    var age = $("input[name='age']",this).val();
-    var pushups = $("input[name='pushups']",this).val();
-    var situps = $("input[name='situps']",this).val();
-    var run_min = $("input[name='run_min']",this).val();
-    var run_sec = $("input[name='run_sec']",this).val();
+    var age = $("input[name='age']", this).val();
+    var pushups = $("input[name='pushups']", this).val();
+    var situps = $("input[name='situps']", this).val();
+    var run_min = $("input[name='run_min']", this).val();
+    var run_sec = $("input[name='run_sec']", this).val();
 
     if (run_sec < 10) {
       run_sec = "0" + String(run_sec);
@@ -224,47 +291,58 @@ $(document).ready(function () {
     var run_row = get_run_row(run_min, run_sec);
     var run_points = get_run_score(age_group, run_row);
 
-    $("#points").empty().prepend(s + " POINTS")
-    $("#age").empty().prepend(age)
+    $("#points")
+      .empty()
+      .prepend(s + " POINTS");
+    $("#age").empty().prepend(age);
     $("#pushup_num").empty().prepend(pushups);
     $("#pushup_points").empty().prepend(push_up_points);
-    $("#pushup_rec").empty().prepend(get_add_push_up(age_group, pushups, push_up_points));
+    $("#pushup_rec")
+      .empty()
+      .prepend(get_add_push_up(age_group, pushups, push_up_points));
     $("#situp_num").empty().prepend(situps);
     $("#situp_points").empty().prepend(sit_up_points);
-    $("#situp_rec").empty().prepend(get_add_sit_up(age_group, situps, sit_up_points));
-    $("#run_num").empty().prepend(run_min + " : " + run_sec);
+    $("#situp_rec")
+      .empty()
+      .prepend(get_add_sit_up(age_group, situps, sit_up_points));
+    $("#run_num")
+      .empty()
+      .prepend(run_min + " : " + run_sec);
     $("#run_points").empty().prepend(run_points);
-    $("#run_rec").empty().prepend(get_run_time_cut(age_group, run_min, run_sec, run_row, run_points));
+    $("#run_rec")
+      .empty()
+      .prepend(
+        get_run_time_cut(age_group, run_min, run_sec, run_row, run_points)
+      );
 
     if (s >= 85) {
-      $("#gold").css({ 'display': 'inline' });
+      $("#gold").css({ display: "inline" });
     } else if (s >= 75) {
-      $("#silver").css({ 'display': 'inline' });
+      $("#silver").css({ display: "inline" });
     } else if (s >= 61) {
-      $("#pass").css({ 'display': 'inline' });
+      $("#pass").css({ display: "inline" });
     } else {
-      $("#fail").css({ 'display': 'inline' });
+      $("#fail").css({ display: "inline" });
     }
-    
   });
 });
 
 $("#cal_agn").click(function () {
   $("form").show();
   $("#result").hide();
-  $("#gold").css({ 'display': 'none' });
-  $("#silver").css({ 'display': 'none' });
-  $("#pass").css({ 'display': 'none' });
-  $("#fail").css({ 'display': 'none' });
+  $("#gold").css({ display: "none" });
+  $("#silver").css({ display: "none" });
+  $("#pass").css({ display: "none" });
+  $("#fail").css({ display: "none" });
 });
 
 $("#brand_link").click(function () {
   $(".cal").hide();
-  $("#cal_link").removeClass('active');
+  $("#cal_link").removeClass("active");
   $(".rewards").hide();
-  $("#rewards_link").removeClass('active');
+  $("#rewards_link").removeClass("active");
   $(".home").show();
-  $("#home_link").addClass('active');
+  $("#home_link").addClass("active");
 });
 
 $("#home_link").click(function () {
@@ -298,8 +376,11 @@ document.getElementById("signin").addEventListener("click", () => {
       //document.getElementById("signout").style.visibility = 'visible';
       $(".signin").hide();
       $(".signout").show();
-      $("#welcome").append(user.displayName);
-    }).catch((error) => {
+      $("#welcome")
+        .empty()
+        .prepend("Welcome " + user.displayName);
+    })
+    .catch((error) => {
       // Handle Errors here.
       const errorCode = error.code;
       const errorMessage = error.message;
@@ -312,21 +393,40 @@ document.getElementById("signin").addEventListener("click", () => {
 });
 
 document.getElementById("signout").addEventListener("click", () => {
-  signOut(auth).then(() => {
-    // Sign-out successful.
-    //document.getElementById("signin").style.visibility = 'visible';
-    //document.getElementById("signout").style.visibility = 'hidden';
-    $(".signin").show();
-    $(".signout").hide();
-  }).catch((error) => {
-    // An error happened.
-  });
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+      //document.getElementById("signin").style.visibility = 'visible';
+      //document.getElementById("signout").style.visibility = 'hidden';
+      $(".signin").show();
+      $(".signout").hide();
+    })
+    .catch((error) => {
+      // An error happened.
+    });
 });
 
-onAuthStateChanged(auth, user => {
+onAuthStateChanged(auth, (user) => {
   if (user != null) {
-    console.log('logged in!');
+    console.log("logged in!");
+    user_id = user.uid;
+    setDoc(doc(db, "users", user_id), {});
+    const querySnapshot = getDocs(
+      collection(db, "users", user_id, "activities")
+    )
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          allEvents.push(doc.data());
+        });
+      })
+      .then(() => {
+        initCalendar(allEvents); // re-initialise calendar upon login
+      });
   } else {
-    console.log('No user');
+    console.log("No user");
+    user_id = null;
+    allEvents = [];
+    initCalendar(allEvents);
   }
 });
